9e22d0b053a938547908ed97d2925d2590e84dec
Johnny Greco <jgreco.ai@gmail.com>
2026-03-13 16:46:08 -0400

    Label search score as BM25 to indicate the scoring algorithm

--- internal/views/components/search_result_templ.go
+++ internal/views/components/search_result_templ.go
@@ -121,14 +121,14 @@ func SearchResultCard(r views.SearchResult) templ.Component {
 			}
 		}
 		if r.Score > 0.01 {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "<span class=\"text-xs text-gray-500\" title=\"BM25 relevance score\">Score: ")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "<span class=\"text-xs text-gray-500\" title=\"BM25 full-text search relevance score\">BM25: ")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var8 string
 			templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%.2f", r.Score))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/views/components/search_result.templ`, Line: 23, Col: 43}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/views/components/search_result.templ`, Line: 23, Col: 42}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 			if templ_7745c5c3_Err != nil {
